29f7e364108eaef51cce267f4521dc91c788dde2
Kurt Endless <kendis_it@hotmail.com>
2019-11-11 23:07:43 +0200

    add: method update profile

--- src/Controllers/ProfilesController.cs
+++ src/Controllers/ProfilesController.cs
@@ -1,8 +1,8 @@
 using AutoMapper;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Linq;
 
 using SoftwareRequirements.Db;
 using SoftwareRequirements.Models.Db;
@@ -32,6 +32,32 @@ namespace SoftwareRequirements.Controllers
             
             var profileView = mapper.Map<Requirement, ProfileView>(profile);
             return Ok(profileView.Profile);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateProfileById(int id, [FromBody]string profile)
+        {
+            var requirement = await db.Requirements.FirstOrDefaultAsync(r => r.Id == id);
+
+            if (requirement == null)
+                return NotFound();
+
+            using (var transaction = await db.Database.BeginTransactionAsync())
+            {
+                try
+                {
+                    requirement.Profile = JsonDocument.Parse(profile);
+                    db.Requirements.Update(requirement);
+                    await db.SaveChangesAsync();
+                    await transaction.CommitAsync();
+                }
+                catch
+                {
+                    await transaction.RollbackAsync();
+                    return StatusCode(500);
+                }
+            }
+            return Ok();
         }
     }
 }
