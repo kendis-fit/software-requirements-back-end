da0ccd13f60665f72ace9b19622f14591bf31d24
Kurt Endless <kendis_it@hotmail.com>
2020-01-02 16:14:03 +0200

    add: new model

---
+++ src/Models/DTO/ProfileRadarResult.cs
@@ -0,0 +1,9 @@
+namespace SoftwareRequirements.Models.DTO
+{
+    public class ProfileRadarResult
+    {
+        public string Name { get; set; }
+
+        public float? Value { get; set; }
+    }
+}
